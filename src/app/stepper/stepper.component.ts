26a5ce6624cdf47c038081006b04624e5eb4b057
Jeff <Haske107@mail.chapman.edu>
2019-05-25 15:33:42 -0700

    Email Forms v1

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-stepper',
@@ -8,8 +8,18 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class StepperComponent implements OnInit {
 
+  // STATE VARIABLES
   Current_Question;
 
+  // CREDENTIAL VARIABLES
+  Details: FormGroup;
+
+
+  Email = "";
+  Password = "";
+  Password_Confirm = "";
+
+  // QUESTION VARIABLES
   Q1_A = "";
   Q2_A = {
     Country: "",
@@ -19,14 +29,33 @@ export class StepperComponent implements OnInit {
   Q3_A;
   Q4_A;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor() {}
+
+  // GETTERS AND SETTERS
+  get email() { return this.Details.get( 'email' ); }
+  get password() { return this.Details.get( 'password' ); }
+  get password_confirm() { return this.Details.get( 'password_confirm' ); }
 
   ngOnInit() {
-    this.Current_Question = 1;
+    this.Current_Question = 0;
+
+    this.Details =  new FormGroup ({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required
+      ]),
+      password_confirm: new FormControl('', [
+        Validators.required
+      ])
+    });
+
   }
 
   Back()  {
-    if(this.Current_Question > 0) {
+    if(this.Current_Question > 1) {
       this.Current_Question--;
     }
   }
@@ -34,5 +63,15 @@ export class StepperComponent implements OnInit {
     if(this.Current_Question < 6) {
       this.Current_Question++;
     }
+  }
+  Begin() {
+    //send request to server
+      //if approved proceed
+        this.Current_Question++;
+  }
+
+
+  recheck()  {
+    console.log(this.Details.valid);
   }
 }
