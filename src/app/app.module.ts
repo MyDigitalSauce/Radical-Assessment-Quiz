cfda574238146a3d45080a15938ced0d362f5f20
Jeff <Haske107@mail.chapman.edu>
2019-05-25 00:40:28 -0700

    e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StepperComponent } from './stepper/stepper.component';
+import {
+  MatButtonModule, MatCardModule, MatInputModule, MatProgressBarModule, MatSliderModule,
+  MatStepperModule,
+} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    StepperComponent,
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatSliderModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
